766324b40dc92741a3993a1d88aace224956c238
muthahar <muthahar.msd@outlook.com>
2016-07-07 23:40:58 -0500

    This is third change by fetching remote, creating a branch tp checkout and make changes, commit and push this third change branch

--- antsample/src/com/gitant/antsample/gitsample.java
+++ antsample/src/com/gitant/antsample/gitsample.java
@@ -6,6 +6,7 @@ public class gitsample {
 		// TODO Auto-generated method stub
 System.out.println("Muthahar Test demo");
 System.out.println("Muthahar created git repository");
+System.out.println("Third change by creating a new branch from remote tracking");
 	}
 
 }
