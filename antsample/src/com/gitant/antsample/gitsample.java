4adf7812397d4baf4796b084754e092345da73f0
muthahar <muthahar.msd@outlook.com>
2016-07-07 22:05:48 -0500

    First commit

---
+++ antsample/src/com/gitant/antsample/gitsample.java
@@ -0,0 +1,10 @@
+package com.gitant.antsample;
+
+public class gitsample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Muthahar Test demo");
+	}
+
+}
