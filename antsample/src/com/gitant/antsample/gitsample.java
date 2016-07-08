8d10aed3a47f4ec4da645e0a4a66f25dfc41d8e9
muthahar <muthahar.msd@outlook.com>
2016-07-07 22:33:08 -0500

    This is second commit change

--- antsample/src/com/gitant/antsample/gitsample.java
+++ antsample/src/com/gitant/antsample/gitsample.java
@@ -5,6 +5,7 @@ public class gitsample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Muthahar Test demo");
+System.out.println("Muthahar created git repository");
 	}
 
 }
